0ef46fdaf5677fdb56a9e137801b742664b1ae7b
OD209 <79770035+OD209@users.noreply.github.com>
2021-12-04 01:28:42 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,6 +4,7 @@ from discord.utils import get
 from discord import channel
 from discord import role
 
+#This token has been disabled
 TOKEN = "OTE1OTg0NjIxNjcyNDY4NTMw.YajjJw._fw2jeLHOM6ZFr3S4rHLBoGAUQ0"
 
 client = discord.Client()
@@ -26,6 +27,7 @@ async def on_message(message):
 
     if role in userroles:
         print("woman detected")
+        #if you want to use a custom emoji on your server you will need to find it's name and id and put it in the format <:name:id>
         emoji = "<:ellie:740572588819873793>"
         await message.add_reaction(emoji)
 
