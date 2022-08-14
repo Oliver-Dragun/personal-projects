7620d5866f77f00a85c3f57da7ebda826a40b657
OD209 <79770035+OD209@users.noreply.github.com>
2022-08-14 20:35:37 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,6 +9,7 @@ TOKEN = "OTE1OTg0NjIxNjcyNDY4NTMw.YajjJw._fw2jeLHOM6ZFr3S4rHLBoGAUQ0"
 
 client = discord.Client()
 
+#This code looks checks if the author of a discord message has the admin role, if they have is it reacts to the comment with a custom emoji 
 
 @client.event
 async def on_ready():
@@ -22,13 +23,11 @@ async def on_message(message):
 
     userroles = message.author.roles
 
-    role = discord.utils.find(
-        lambda r: r.name == 'women', message.guild.roles)
+    role = discord.utils.find(lambda r: r.name == 'admin', message.guild.roles)
 
     if role in userroles:
-        print("woman detected")
         #if you want to use a custom emoji on your server you will need to find it's name and id and put it in the format <:name:id>
-        emoji = "<:ellie:740572588819873793>"
+        emoji = "<:admin:740572588819873793>"
         await message.add_reaction(emoji)
 
 client.run(TOKEN)
